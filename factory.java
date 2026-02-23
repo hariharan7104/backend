4478e7157cc40f1d6d08b252380f5d61f890375b
Hariharan <hariharan59988@gmail.com>
2026-02-23 15:30:12 +0530

    final

--- factory.java
+++ factory.java
@@ -1,6 +1,43 @@
 import java.util.*;
+interface notification{
+   public void message();
+}
+class Sms implements notification{
+   public void message(){
+        System.out.print("sms");
+    }
+}
+class Email implements notification{
+    public void message(){
+        System.out.print("email");
+    }
+}
+class Push implements notification{
+    public void message(){
+        System.out.print("push");
+    }
+}
+class Sfactory{
+    public static notification sample(String type){
+        if(type.equalsIgnoreCase("sms")){
+            return new Sms();
+        }
+        else if(type.equalsIgnoreCase("email")){
+            return new Email();
+        }
+        else if(type.equalsIgnoreCase("push")){
+            return new Push();
+        }
+        else{
+            throw new IllegalArgumentException("Invalid service");
+        }
+    }
+}
 class factory{
     public static void main(String args[]){
-        System.out.print("hello");
+        Scanner inp=new Scanner(System.in);
+        String type=inp.nextLine();
+        notification n=Sfactory.sample(type);
+        n.message();
     }
 }
